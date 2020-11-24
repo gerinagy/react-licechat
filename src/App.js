b0dce79745bcf21ce14be52f92db6fff070aab0d
gerinagy <nagy.gergi6@gmail.com>
2020-11-24 22:22:56 +0100

    fresh

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDCzcukPLGnEPThI2_86hIeqHXTHjv4HLs",
+  authDomain: "react-livechat-9c29d.firebaseapp.com",
+  databaseURL: "https://react-livechat-9c29d.firebaseio.com",
+  projectId: "react-livechat-9c29d",
+  storageBucket: "react-livechat-9c29d.appspot.com",
+  messagingSenderId: "1034760244529",
+  appId: "1:1034760244529:web:ea91f5ac20bf341e0e7410",
+  measurementId: "G-7YBZ2S785F"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.girestore();
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
