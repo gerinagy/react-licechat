8a737206c72bfc0a35195a6e6a9ccd89864101db
gerinagy <nagy.gergi6@gmail.com>
2020-11-25 09:00:02 +0100

    bug fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-import { useAuthState } from "react-firebase-hooks/auth";
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 
@@ -16,24 +16,84 @@ firebase.initializeApp({
   projectId: "react-livechat-9c29d",
   storageBucket: "react-livechat-9c29d.appspot.com",
   messagingSenderId: "1034760244529",
-  appId: "1:1034760244529:web:ea91f5ac20bf341e0e7410",
-  measurementId: "G-7YBZ2S785F"
+  appId: "1:1034760244529:web:ea91f5ac20bf341e0e7410"
 })
 
 const auth = firebase.auth();
-const firestore = firebase.girestore();
+const firestore = firebase.firestore();
 
+const [user] = useAuthState(auth);
 
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
+      <header className="App-Header">
+
+
       </header>
+
+      <section className="App-main">
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
 }
+
+
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+  
+
+
+  return (
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+
+  );
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+
+    <button onClick={ () => auth.signOut()}>Sign Out</button>
+  );
+}
+
+
+function ChatRoom() {
+
+  const messagesRef = firebase.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, {idField: 'id'});
+
+
+  return(
+    <>
+      <div>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} /> )}
+      </div>
+    
+    </>
+  )
+}
+
+
+function ChatMessage(props) {
+  const { text, uid } = props.message;
+
+
+
+  return <p>{text}</p>
+}
+
+
+
 
 export default App;
