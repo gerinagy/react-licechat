696860abc94c46ceacb719e899c19619eb9e1fec
gerinagy <nagy.gergi6@gmail.com>
2021-01-08 18:32:26 +0100

    image fix

--- src/App.js
+++ src/App.js
@@ -142,14 +142,14 @@ function ChatRoom() {
 
 
 function ChatMessage(props) {
-  const { text, uid } = props.message;
+  const { text, uid, photoURL} = props.message;
 
   const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
   return (
     <div className={`message ${messageClass}`}>
 
-      <img src="{photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'}}" alt="no" />
+      <img src={photoURL} alt="no" />
 
       <p>{text}</p>
 
